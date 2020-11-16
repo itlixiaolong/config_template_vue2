eedb3c597b16253dfba6f1a07ec35fe969acb804
lixiaolong02 <lixiaolong02@longfor.com>
2020-11-16 14:43:14 +0800

    fix(test): ggdfsjd

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 <script>
 export default {
   name: 'App',
+  props: {},
   data() {
     return {}
   },
---
+++ src/testd.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="testd">
+    <!--html code-->
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Testd',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+.testd {
+  width: 100%;
+
+  /* less */
+  font-size: 10px;
+}
+
+</style>
